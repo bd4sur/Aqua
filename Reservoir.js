414e190a3cfb0ecbc2e0f99d0134739c5659a005
憂鬱的烏龜 <20069428+mikukonai@users.noreply.github.com>
2019-12-23 00:52:26 +0800

    继续研究比特储备池机制

--- Reservoir.js
+++ Reservoir.js
@@ -1,8 +1,25 @@
 /**
- * @description 全局变量：比特池，单位为bit
+ * @description 全局变量：比特储备池，单位为bit
  */
 let RESERVOIR_MAX  = 0;
 let RESERVOIR_SIZE = 0;
+
+/**
+ * @description 每帧开始时调用，同dist10的ResvFrameBegin
+ */
+function ReservoirFrameBegin(meanBitsPerGranule, frameLength) {
+    // 根据当前帧长度修改比特储备池最大长度
+    if(frameLength > 7680) { // NOTE 7680是320k/48kHz的帧比特数（320000*1152/48000=7680）
+        RESERVOIR_MAX = 0;
+    }
+    else {
+        RESERVOIR_MAX = 7680 - frameLength;
+    }
+    // 因为mainDataBegin为9bit，因此最多能表示511*8=4088bit的比特储备池
+    if(RESERVOIR_MAX > 4088) {
+        RESERVOIR_MAX = 4088;
+    }
+}
 
 /**
  * @description 计算内层循环的每个Granule的比特数限制
@@ -32,7 +49,7 @@ function ReservoirMaxBits(perceptualEntropy, meanBitsPerGranule) {
 }
 
 /**
- * @description 编码一个Granule后，将剩余的比特捐献给比特池
+ * @description 编码一个Granule后，将剩余的比特捐献给比特储备池
  */
 function ReservoirAdjust(part23Length, meanBitsPerGranule) {
     RESERVOIR_SIZE += (meanBitsPerGranule / CHANNELS) - part23Length;
