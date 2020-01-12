14f5815244f77745d7ae4acc01c29873325e1c50
憂鬱的烏龜 <20069428+mikukonai@users.noreply.github.com>
2020-01-13 00:32:11 +0800

    完善心理声学模型

--- PsychoacousticModel2.js
+++ PsychoacousticModel2.js
@@ -1,11 +1,71 @@
 
-// 第二心理声学模型 试验性实现
+// 第二心理声学模型（PAM2）
 
+////////////////////////////////
+//
+//  缓 冲 区
+//
+////////////////////////////////
 
-/** @description 前两个长块的极坐标频谱（0~1023）*/
+// 前两个长块的极坐标频谱（0~1023）
 let PrevLongPolarSpectrum1, PrevLongPolarSpectrum2;
-/** @description 前两个短块的极坐标频谱（0~255）*/
+// 前两个短块的极坐标频谱（0~255）
 let PrevShortPolarSpectrum1, PrevShortPolarSpectrum2;
+
+// 前两个长块的未考虑前回声效应的归一化阈值（以TCP为下标），即nb[b]
+let PrevNB1, PrevNB2;
+
+////////////////////////////////
+//
+//  心 理 声 学 模 型 参 数
+//
+////////////////////////////////
+
+const SWITCH_PE = 1800; // @reference p95
+
+let TABLE_C7_LONG  = PAM_TABLES_C7[SAMPLE_RATE][LONG_BLOCK];
+let TABLE_C7_SHORT = PAM_TABLES_C7[SAMPLE_RATE][SHORT_BLOCK];
+let TABLE_C8_LONG  = PAM_TABLES_C8[SAMPLE_RATE][LONG_BLOCK];
+let TABLE_C8_SHORT = PAM_TABLES_C8[SAMPLE_RATE][SHORT_BLOCK];
+
+// 阈值计算区间：由表C7定义，由PAM2_Init函数初始化之
+let TCPS_LONG  = new Array(); // [[wlow, whigh]]
+let TCPS_SHORT = new Array(); // [[wlow, whigh]]
+
+// 扩散函数
+let SPREADING_FUNCTION = new Array();
+
+/**
+ *  心理声学模型参数初始化
+ */
+function PAM2_Init() {
+
+    // TCPS_LONG init
+    let wcount = 0;
+    for(let b = 0; b < TABLE_C7_LONG.length; b++) {
+        let fft_lines = TABLE_C7_LONG[b][0];
+        TCPS_LONG.push([wcount, wcount + (fft_lines - 1)]);
+        wcount += fft_lines;
+    }
+
+    // TCPS_LONG init
+    wcount = 0;
+    for(let b = 0; b < TABLE_C7_SHORT.length; b++) {
+        let fft_lines = TABLE_C7_SHORT[b][0];
+        TCPS_SHORT.push([wcount, wcount + (fft_lines - 1)]);
+        wcount += fft_lines;
+    }
+
+    // SPREADING_FUNCTION init
+    for(let bi = 0; bi < TABLE_C7_LONG.length; bi++) {
+        SPREADING_FUNCTION[bi] = new Array();
+        for(let bj = 0; bj < TABLE_C7_LONG.length; bj++) {
+            SPREADING_FUNCTION[bi][bj] = SpreadingFunction(bi, bj);
+        }
+    }
+
+}
+
 
 /**
  * @description 计算扩散函数c(w)
@@ -133,10 +193,147 @@ function CalculateUnpredictability(
     return c;
 }
 
+/**
+ * Part1
+ */
+function CalculateRatiosAndRE(polarSpectrum, Unpred_w) {
 
-class PsychoacousticModel2 {
-    constructor() {
+    // 最大的 TCP index （bmax）
+    let bmax = TCPS_LONG.length - 1;
 
+    // (e) 计算阈值计算区间（Threshold Calculation Partition）内的能量、以及不可预测度加权的能量
+
+    let Energy = new Array();
+    let Unpred = new Array();
+
+    for(let b = 0; b < TABLE_C7_LONG.length; b++) {
+        let ebsum = 0;
+        let cbsum = 0;
+        for(let w = TCPS_LONG[b][0]; w <= TCPS_LONG[b][1]; w++) {
+            let rw = polarSpectrum[0][w];
+            ebsum += (rw * rw);
+            cbsum += (rw * rw * Unpred_w[w]);
+        }
+        Energy[b] = ebsum;
+        Unpred[b] = cbsum;
     }
+
+    // (f) 与扩散函数作卷积
+
+    let ecb = new Array();
+    let ct = new Array();
+
+    for(let b = 0; b < TABLE_C7_LONG.length; b++) {
+        let ecb_sum = 0;
+        let ct_sum = 0;
+        for(let bb = 0; bb < TABLE_C7_LONG.length; bb++) {
+            let bval_bb = TABLE_C7_LONG[bb][4]; // bval在TABLE_C7_LONG表中的下标是4
+            let bval_b  = TABLE_C7_LONG[b][4];
+            let sprdngf = SPREADING_FUNCTION[bval_bb][bval_b];
+            ecb_sum += (Energy[bb] * sprdngf);
+            ct_sum  += (Unpred[bb] * sprdngf);
+        }
+        ecb[b] = ecb_sum;
+        ct[b] = ct_sum;
+    }
+
+    // 将ct[b]按照ecb[b]归一化，得到cb[b]
+
+    let cb = new Array();
+
+    for(let b = 0; b < TABLE_C7_LONG.length; b++) {
+        if(ecb[b] === 0) {
+            cb[b] = 0;
+        }
+        else {
+            cb[b] = ct[b] / ecb[b];
+            if(cb[b] < 0.01) cb[b] = 0.01; // 参考dist10
+        }
+    }
+
+    // 将ecb[b]归一化，得到en[b] （Layer3所用的PAM2不需要这一步）
+
+    // (g) 计算每个TCP的纯音指数tb
+
+    let tb = new Array();
+
+    for(let b = 0; b < TABLE_C7_LONG.length; b++) {
+        tb[b] = -0.299 - 0.43 * Math.log(cb[b]);
+        tb[b] = Math.min(1.0, Math.max(0, tb[b])); // 取值范围 (0,1)
+    }
+
+    // (h) 计算每个TCP的SNR
+
+    let SNR = new Array();
+
+    const NMT = 6.0;   // (in dB) Noise masking tone. @reference C.1.5.3.2.1(p80)
+    const TMN = 29.0;  // (in dB) Tone masking noise. @reference C.1.5.3.2.1(p80)
+
+    for(let b = 0; b < TABLE_C7_LONG.length; b++) {
+        let minval = TABLE_C7_LONG[b][1]; // minval在TABLE_C7_LONG表中的下标是1
+        SNR[b] = Math.max(minval, tb[b] * TMN + (1.0 - tb[b]) * NMT);
+    }
+
+    // (i)(j) 计算能量阈值nb
+
+    let nb = new Array();
+
+    for(let b = 0; b < TABLE_C7_LONG.length; b++) {
+        let norm = TABLE_C7_LONG[b][3]; // norm在TABLE_C7_LONG表中的下标是3
+        nb[b] = ecb[b] * norm * Math.pow(10, (-SNR[b] / 10)); // NOTE 标准附录C的算法框图中，此处少了一个符号。（以dist10源码为准）
+    }
+
+    // 以下不采用附录D的算法，而是采用附录C给出的算法（见p81、p92）
+
+    // 计算每个TCP的阈值thr[b]（前回声控制）
+
+    let thr = new Array();
+
+    const rpelev  = 2;  // @reference C.1.5.3.2.1(p81)
+    const rpelev2 = 16; // @reference C.1.5.3.2.1(p81)
+
+    for(let b = 0; b < TABLE_C7_LONG.length; b++) {
+        let qthr = TABLE_C7_LONG[b][2]; // qthr在TABLE_C7_LONG表中的下标是1
+        let nb_l  = PrevNB1[b] * rpelev;     // @reference p92
+        let nb_ll = PrevNB2[b] * rpelev2;    // @reference p92
+        // NOTE 根据dist10，标准文档p92有误，以下以dist10为准
+        let min_nb = Math.min(nb[b], nb_l, nb_ll); // 标准文档是取大者
+        thr[b] = Math.max(qthr, min_nb);
+    }
+
+    // 将thr[b]通过表C8直接转换到尺度因子频带上
+
+    let en_Sb = new Array();
+    let thr_Sb = new Array();
+
+    // TODO
+
+    // 计算每个尺度因子频带的最终的ratio
+
+    let ratio_Sb = new Array();
+
+    // TODO
+
+    // 计算感知熵（PE）
+
+    let PE = 0;
+
+    for(let b = 0; b < TABLE_C7_LONG.length; b++) {
+        let pe_b = - Math.min(0.0, Math.log((thr[b] + 1.0) / (Energy[b] + 1.0)));
+        let lines = TABLE_C7_LONG[b][0]; // lines在TABLE_C7_LONG表中的下标是0
+        PE += (pe_b * lines);
+    }
+
+    // 处理缓冲区：将本帧的nb保存到缓冲区中
+
+    PrevNB2 = PrevNB1;
+    PrevNB1 = nb;
+
+    // 返回结果
+
+    return {
+        "ratio": ratio_Sb,
+        "PE": PE
+    };
 
 }
